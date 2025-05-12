faae6a25273a4df2722afe75f266db466362fd7a
Tony <antony.branquinho@gmail.com>
2025-05-12 16:55:31 -0300

    Coloquei mais uma imagem e organizei melhor o codigo

--- ExercicioResolvidoAula101/Program.cs
+++ ExercicioResolvidoAula101/Program.cs
@@ -13,31 +13,28 @@ namespace MyApp
             // Solicita e armazena o DEPARTAMENTO.
             Console.Write("INSIRA O NOME DO DEPARTAMENTO: ");
             string depName = Console.ReadLine();
-            // Cria o objeto departamento conforme a classe Department.
-            Department departamento = new Department(depName);
-
-
             // Solicita e armazena dados do trabalhador.
             Console.WriteLine("INSIRA OS DADOS DO TRABALHADOR:");
             Console.Write("NOME: ");
             string name = Console.ReadLine();
-
             Console.Write("NIVEL DO TRABALHADOR (Junior, Pleno, Senior): ");
             string entrada = Console.ReadLine();
             // Declara uma variavel level do tipo WorkerLevel mas nao atribui valor ainda.
             WorkerLevel level;
             // Testa se o que o usuario digitou corresponde a alguma das opçoes.
             bool conversaoValida = Enum.TryParse(entrada, out level);
-
             Console.Write("SALARIO BASE: ");
             double baseSalary = double.Parse(Console.ReadLine());
-            Console.Write("Quantos contratos para este trabalhador?");
-            int addCont = int.Parse(Console.ReadLine());
 
+
+            // Cria o objeto departamento conforme a classe Department.
+            Department departamento = new Department(depName);
             // CRIA UM OBJETO TRABALHADOR
             Worker trab = new Worker(name, level, baseSalary, departamento);
 
 
+            Console.Write("Quantos contratos para este trabalhador?");
+            int addCont = int.Parse(Console.ReadLine());
 
             // CADASTRA OS DADOS EM CADA UM DOS CONTRATOS
             for (int i = 0; i < addCont; i++)
@@ -54,28 +51,13 @@ namespace MyApp
                 trab.AddContract(contrato);
             }
 
-
-
-
-
-
             Console.Write("Digite o mes e ano para verificacao do salario(MM/YYYY):");
             string entrada2 = Console.ReadLine();
-
             DateTime periodo;
-
             // TENTA CONVERTER A STRING EM DATA.
             bool conversaoValida2 = DateTime.TryParse("01/" + entrada2, out periodo);
-
             int month = periodo.Month;
             int year = periodo.Year;
-
-
-
-
-
-
-
             Console.Write("NOME: " + trab.Name);
             Console.Write("DEPARTAMENTO: " + trab.Department);
             Console.Write("QUANTIA RECEBIDA EM " + entrada2 + ": " + trab.Recebido(year, month));
